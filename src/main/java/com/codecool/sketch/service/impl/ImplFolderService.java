75398ea424208a954d88754434152d84bc6fb8ab
szucsaron <aron.szucs.miskolc@gmail.com>
2019-05-21 18:23:30 +0200

    ImplFolderService created

---
+++ src/main/java/com/codecool/sketch/service/impl/ImplFolderService.java
@@ -0,0 +1,26 @@
+package com.codecool.sketch.service.impl;
+
+import com.codecool.sketch.dao.FolderDao;
+import com.codecool.sketch.model.Folder;
+import com.codecool.sketch.model.User;
+import com.codecool.sketch.service.FolderService;
+import com.codecool.sketch.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ImplFolderService extends AbstractService implements FolderService {
+    private FolderDao folderDao;
+    public ImplFolderService(User user, FolderDao folderDao) {
+        super(user);
+        folderDao = folderDao;
+    }
+
+    public List<Folder> fetchAll() throws ServiceException, SQLException {
+        if (adminMode) {
+            return null;
+        } else {
+            return folderDao.fetchByUserId(fetchUserId());
+        }
+    }
+}
